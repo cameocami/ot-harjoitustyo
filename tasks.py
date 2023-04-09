539975e9ee13769790ad14baf90f11e8ded12990
Camilla Campbell <camilla.campbell@helsinki.fi>
2023-04-09 10:02:43 +0300

    fixing invoke start task

--- tasks.py
+++ tasks.py
@@ -1,12 +1,12 @@
 from invoke import task
 
 @task
-def foo(ctx):
-    print("bar")
-
-@task
 def start(ctx):
     ctx.run("python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("poetry run invoke test", pty=True)
 
 @task
 def coverage(ctx):
