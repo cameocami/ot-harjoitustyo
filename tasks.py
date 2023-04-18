6d98bf0ab992fb4e4844cd2924dc95ad1475519a
cameocami <118991446+cameocami@users.noreply.github.com>
2023-04-18 12:21:53 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -6,7 +6,15 @@ def start(ctx):
 
 @task
 def test(ctx):
-    ctx.run("poetry run invoke test", pty=True)
+    ctx.run("pytest src", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+@task
+def format(ctx):  # pylint: disable=redefined-builtin
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
 
 @task
 def coverage(ctx):
