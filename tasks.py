b717c6a72b371dde41d1e7dd1d0c496239ac2abe
cameocami <118991446+cameocami@users.noreply.github.com>
2023-04-18 15:42:03 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -18,7 +18,7 @@ def format(ctx):  # pylint: disable=redefined-builtin
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
