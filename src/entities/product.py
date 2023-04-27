939b3cab33f9557e22387bc1ec8f3e3e23bcb5eb
Camilla Campbell <camilla.campbell@helsinki.fi>
2023-04-27 16:25:32 +0300

    add docstring to entities

--- src/entities/product.py
+++ src/entities/product.py
@@ -2,10 +2,26 @@ from entities.department import Department
 
 
 class Product:
+    """Class responsible for keeping track of the department of a product.
+
+    Attributes:
+            name (str): product name
+            department (Department): department in which product is found
+    """
     def __init__(self, name: str, department: Department):
+        """Class constructor, that creates a new product 
+        Args:
+            name (str): product name
+            department (Department): department in which product is found
+        """
         self.name = name.lower()
         self.department = department
 
     def __repr__(self):
+        """ Forms a string formatted representation for the product.
+
+        Returns:
+            string : product name capitalized
+        """
         text = f'{self.name.capitalize()}'
         return text
--- src/entities/store.py
+++ src/entities/store.py
@@ -1,15 +1,25 @@
 class Store:
+    """Class responsible for keeping track of the department order in a store.
+
+    Attributes:
+            name (str): store name
+            departments (list): list of departments in the order which they appear in the store
+    """
+    
     def __init__(self, name: str, departments: list):
+        """Class constructor.
+
+        Args:
+            name (str): store name
+            departments (list): list of departments in the order which they appear in the store
+        """
         self.name = name
         self.departments = departments
 
     def get_department_order_in_store(self):
+        """Retrieves the list of departments in order, which is a hidden attribute.
+
+        Returns:
+            list: departments in the order which they appear in the store
+        """
         return self.departments
-
-    ''' future developments
-    def rearrange_departments(self):
-        pass
-
-    def __repr__(self):
-        return f'{self.name}: {self.departments}'
-    '''
